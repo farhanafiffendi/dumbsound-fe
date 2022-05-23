3edddbb0a2c869d6f4e76909257b02e7ecaf852d
farhanafiffendi <farhanafiffendi19@gmail.com>
2022-05-23 18:51:56 +0700

    update

--- src/App.js
+++ src/App.js
@@ -11,7 +11,6 @@ import AddMusic from './pages/Admin/AddMusic';
 import MenuTransaksi from './pages/User/MenuTransaksi'
 import Complain from './pages/Complain';
 import ComplainAdmin from './pages/ComplainAdmin';
-import HomePaySuccess from './pages/User/HomePaySuccess';
 
 import { setAuthToken, API } from './config/api';
 
@@ -80,7 +79,6 @@ function App() {
       <Route path='/add-artist' element={<AddArtist />} />
       <Route path='/pay' element={<MenuTransaksi />} />
       <Route path='/' element={<HomeSuccess />} />
-      <Route path='/dumbsound' element={<HomePaySuccess />} />
       <Route path='/complain' element={<Complain />} />
       <Route path='/complain-admin' element={<ComplainAdmin />} />
     </Routes>
--- src/pages/User/HomeSuccess.js
+++ src/pages/User/HomeSuccess.js
@@ -1,12 +1,40 @@
-import React from 'react'
+import React, { useState, useContext, useEffect } from 'react'
 import HeaderHome from '../../components/HeaderHome'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import AudioPlayer from 'react-h5-audio-player';
+import 'react-h5-audio-player/lib/styles.css';
+import song from '../../components/assets/adele.mp3'
 import NavbarAdmin from '../../components/NavbarAdmin';
 import { Link } from 'react-router-dom';
 import { API } from '../../config/api'
 import { useQuery } from "react-query";
+import { UserContext } from '../../context/userContext';
 
 export default function HomeSuccess() {
+
+    const [state, dispatch] = useContext(UserContext);
+
+    const [userTrans, setUserTrans] = useState({})
+    console.log(userTrans);
+
+    const loadUserTrans = async () => {
+        try {
+            const config = {
+                headers: {
+                    Authorization: "Basic " + localStorage.token,
+                    "Content-type": "application/json",
+                },
+            };
+            const response = await API.get(`userTrans/${state.user.id}`, config)
+            setUserTrans(response.data.data.user.transaction)
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    useEffect(() => {
+        loadUserTrans()
+    }, [])
 
     // Fetching product data from database
     let { data: musics, refetch } = useQuery("musicsCache", async () => {
@@ -36,33 +64,70 @@ export default function HomeSuccess() {
                 <span className='header-card text-center'><p>Dengarkan Dan Rasakan</p></span>
                 <div>
                     <div className='d-flex flex-wrap justify-content-start ms-4 mt-5'>
-                        {/* ================= Ketika User Belum Pernah Transaksi ==========*/}
-                        <>
-                            <Link to='/pay' className='none-item'>
-                                {musics?.map((item, index) => {
-                                    return (
-                                        <div className='card-item mb-3 me-3'>
-                                            <div className='card-item-header'>
-                                                <img src={item.thumbnail} alt="" />
+                        {/* ================= Ketika User Belum Bayar Atau Status pending ==========*/}
+                        {userTrans === null ?
+                            <>
+                                <Link to='/pay' className='none-item'>
+                                    {musics?.map((item, index) => {
+                                        return (
+                                            <div className='card-item mb-3 me-3'>
+                                                <div className='card-item-header'>
+                                                    <img src={item.thumbnail} alt="" />
+                                                </div>
+                                                <div className="text-card">
+                                                    <span className='text-title'>
+                                                        <p>{item.title}</p>
+                                                    </span>
+                                                    <p>{item.year}</p>
+                                                </div>
+                                                <div className='flex-start'>
+                                                    <p>{item.art.name}</p>
+                                                </div>
                                             </div>
-                                            <div className="text-card">
-                                                <span className='text-title'>
-                                                    <p>{item.title}</p>
-                                                </span>
-                                                <p>{item.year}</p>
-                                            </div>
-                                            <div className='flex-start'>
-                                                <p>{item.art.name}</p>
-                                            </div>
-                                        </div>
-                                    )
-                                })}
-                            </Link>
-                        </>
+                                        )
+                                    })}
+                                </Link>
+                            </>
+                            :
+                            //=====================ketika status success===================
+                            <>
+                                {userTrans.status === "success" ?
+                                    <>
+                                        {musics?.map((item, index) => {
+                                            return (
+                                                <div className='card-item mb-3 me-3'>
+                                                    <div className='card-item-header'>
+                                                        <img src={item.thumbnail} alt="" />
+                                                    </div>
+                                                    <div className="text-card">
+                                                        <span className='text-title'>
+                                                            <p>{item.title}</p>
+                                                        </span>
+                                                        <p>{item.year}</p>
+                                                    </div>
+                                                    <div className='flex-start'>
+                                                        <p>{item.art.name}</p>
+                                                    </div>
+                                                </div>
+                                            )
+                                        })}
+
+                                    </>
+                                    :
+                                    <>
+                                    </>
+                                }
+                            </>
+                        }
                     </div>
                 </div>
             </div>
+            <AudioPlayer
+                className='media-player'
+                autoPlay
+                src={song}
+                onPlay={e => console.log("onPlay")}
+            />
         </>
     )
 }
-
