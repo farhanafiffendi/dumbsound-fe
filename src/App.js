f0b94cef303f9a1f57cf747850dc5f115b4b410c
farhanafiffendi <farhanafiffendi19@gmail.com>
2022-05-29 23:13:41 +0700

    update

--- src/App.js
+++ src/App.js
@@ -99,7 +99,6 @@ function App() {
         <Route path='/complain-admin' element={<ComplainAdmin />} />
         <Route path='/music-list' element={<MusicList />} />
         <Route path='/list-transaction' element={<ListTransaction />} />
-        <Route path='*' element={<NotFound />} />
         <Route path='/pay' element={<MenuTransaksi />} />
         <Route path='/' element={<HomeSuccess />} />
         <Route path='/complain' element={<Complain />} />
